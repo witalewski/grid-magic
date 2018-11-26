c9ee468782cd637d9a984ba9da0b2f06cddc37cf
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-11-26 14:59:42 +0100

    use canvas instead of img

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { string } from 'prop-types';
 import { connect } from 'react-redux';
 
-export const Preview = ({ image }) => image && <img alt="Preview" src={image} />;
+export const Preview = ({ image }) => {
+  const canvas = useRef(null);
+
+  const width = (1080 * 3 + 2) / 4;
+  const height = 1080 / 4;
+
+  useEffect(() => {
+    if (image && canvas.current) {
+      const ctx = canvas.current.getContext('2d');
+      const img = new Image();
+      img.onload = () => {
+        ctx.drawImage(img, 0, 0, width, height);
+      };
+      img.src = image;
+    }
+  });
+
+  return image && <canvas ref={canvas} alt="Preview" width={width} height={height} />;
+};
 
 Preview.propTypes = {
   image: string,
