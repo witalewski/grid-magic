280bde212da7d819c671e60ee25664c37b9d8632
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-11-26 16:38:35 +0100

    scale image & display background

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -5,16 +5,40 @@ import { connect } from 'react-redux';
 export const Preview = ({ image }) => {
   const canvas = useRef(null);
 
-  const width = (1080 * 3 + 2) / 4;
-  const height = 1080 / 4;
+  const tileSize = 1080 / 4;
+  const width = tileSize * 3 + 2;
+  const height = tileSize;
+
+  const onImageLoad = ({ target }) => {
+    if (canvas.current) {
+      const ctx = canvas.current.getContext('2d');
+      ctx.fillStyle = 'white';
+      ctx.fillRect(0, 0, width, height);
+      let targetWidth; let targetHeight; let xOffset; let
+        yOffset;
+      const imgProportions = target.width / target.height;
+      const canvasProportions = width / height;
+      if (imgProportions > canvasProportions) {
+        targetWidth = width;
+        targetHeight = width / imgProportions;
+        xOffset = 0;
+        yOffset = (height - targetHeight) / 2;
+      } else {
+        targetWidth = height * imgProportions;
+        targetHeight = height;
+        xOffset = (width - targetWidth) / 2;
+        yOffset = 0;
+      }
+      ctx.drawImage(target, xOffset, yOffset, targetWidth, targetHeight);
+      ctx.fillRect(tileSize, 0, 1, tileSize);
+      ctx.fillRect(tileSize * 2, 0, 1, tileSize);
+    }
+  };
 
   useEffect(() => {
-    if (image && canvas.current) {
-      const ctx = canvas.current.getContext('2d');
+    if (image) {
       const img = new Image();
-      img.onload = () => {
-        ctx.drawImage(img, 0, 0, width, height);
-      };
+      img.onload = onImageLoad;
       img.src = image;
     }
   });
